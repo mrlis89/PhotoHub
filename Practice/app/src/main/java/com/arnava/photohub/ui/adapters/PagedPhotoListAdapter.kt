4f3a00f2c3baeaff9af626cbd5410fa7ec8e465b
mrlis89 <mr.lis89@mail.ru>
2023-01-27 05:08:55 +0700

    glide with async await

--- Practice/app/src/main/java/com/arnava/photohub/ui/adapters/PagedPhotoListAdapter.kt
+++ Practice/app/src/main/java/com/arnava/photohub/ui/adapters/PagedPhotoListAdapter.kt
@@ -2,18 +2,22 @@ package com.arnava.photohub.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.arnava.photohub.App
 import com.arnava.photohub.R
 import com.arnava.photohub.data.models.unsplash.photo.UnsplashPhoto
 import com.arnava.photohub.databinding.PhotoLayoutBinding
 import com.arnava.photohub.utils.common.NetworkState
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
-class PagedPhotoListAdapter (
+class PagedPhotoListAdapter(
     private val onPhotoClick: (UnsplashPhoto) -> Unit,
     private val onLikeClick: (UnsplashPhoto) -> Unit,
 ) : PagingDataAdapter<UnsplashPhoto, PhotoListViewHolder>(PhotoDiffUtilCallback()) {
@@ -33,11 +37,24 @@ class PagedPhotoListAdapter (
                 photoLikeByUser.isSelected = !photoLikeByUser.isSelected
                 photo?.likedByUser = photoLikeByUser.isSelected
             }
-            Glide
-                .with(holder.itemView)
-                .load(photo?.urls?.regular)
-                .placeholder(R.drawable.placeholder_photo)
-                .into(photoView)
+            val image = CoroutineScope(Dispatchers.IO).async {
+                return@async Glide
+                    .with(holder.itemView)
+                    .asBitmap()
+                    .apply(
+                        RequestOptions()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    )
+                    .load(photo?.urls?.regular)
+                    .placeholder(R.drawable.placeholder_photo)
+                    .submit()
+                    .get()
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                val bitmap = image.await()
+                photoView.setImageBitmap(bitmap)
+            }
+
 
             root.setOnClickListener {
                 if (NetworkState.isConnected()) {
@@ -58,5 +75,6 @@ class PhotoDiffUtilCallback : DiffUtil.ItemCallback<UnsplashPhoto>() {
     override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
         oldItem.id == newItem.id
 
-    override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) = oldItem == newItem
+    override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
+        oldItem == newItem
 }
